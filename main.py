c903fab5ff99cc67ab05afc37c902ce157b86e00
Gamer-J <jomiobas@gmail.com>
2024-12-30 11:12:07 -0500

    Target class created

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ WIDTH , HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("AIM TRAINER")
 
+
+class Target:
+    MAX_SIZE = 30
+    GROWTH_RATE = 0.2
+    COLOUR = "red"
+    #constructor
+    #set up the initial state of an object by initializing the attributes or properties of the class.
+    def __init__(Self,x,y): #self allows you to access the attributes and methods of the object. When you create an object from a class, the __init__ method is automatically called.
+        self.x=x
+        self.y=y
+        self.size=0
+        self.grow=True
 def main():
     run = True
     while run:
