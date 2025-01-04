e348106c61ae5746722fbef7a876f54a097f7ac9
Gamer-J <jomiobas@gmail.com>
2025-01-04 11:44:00 -0500

    Update display and finished x and y coordinates

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ WIDTH , HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("AIM TRAINER")
 
+TARGET_INCREMENT = 400
+TARGET_EVENT = pygame.USEREVENT
+TARGET_PADDING = 30  # how many pizels off the center of the screen
 
 class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
     COLOUR = "red"
+    SECOND_COLOUR = "white"
     #constructor
     #set up the initial state of an object by initializing the attributes or properties of the class.
     def __init__(Self,x,y): #self allows you to access the attributes and methods of the object. When you create an object from a class, the __init__ method is automatically called.
@@ -27,13 +31,35 @@ class Target:
             self.size += self.GROWTH_RATE
         else:
             self.grow -= self.GROWTH_RATE
+    def draw(self,win):
+        pygame.draw.circle(win,self.COLOUR,self.x,self.y,self.size)
+        pygame.draw.circle(win,self.SECOND_COLOUR,self.x,self.y,self.size*0.8)
+        pygame.draw.circle(win,self.COLOUR,self.x,self.y,self.size*0.6)
+        pygame.draw.circle(win,self.SECOND_COLOUR,self.x,self.y,self.size*0.4)
+
+def draw(win, targets):
+    win.fill("Black")      # Background colour
+
+    for target in targets:
+        target.draw(win)
+    pygame.display.update()
 def main():
     run = True
+    target = []
+    pygame.time,set_timer(TARGET_EVENT,TARGET_INCREMENT)
     while run:
         for event in pygame.event.get():
             if event.type== pygame.QUIT:
                 run = False
                 break
+            if event.type == TARGET_EVENT:
+                x=random.randint(TARGET_PADDING,WIDTH-TARGET_PADDING) #preventing target from going of screen because of radius
+                y=random.randint(TARGET_PADDING,HEIGHT-TARGET_PADDING)
+                target=Target(x,y)
+                targets.append(target)
+        for target in targets:
+            targetss.update()
+    draw(WIN,targets)
     pygame.quit()
 
 if __name__ == "__main__": #makes sure the function main runs if we are exucuting this file directly
