3c3c7cbf9a89b5a37835a429e55e2c4617945067
Gamer-J <jomiobas@gmail.com>
2025-01-21 10:14:30 -0500

    Top bar with stats added

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ WIDTH , HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("AIM TRAINER")
 
+TOP_BAR_HEIGHT = 50
+LABEL_FONT = pygame.font.SysFont("comicsans",24)
+
 TARGET_INCREMENT = 400
 TARGET_EVENT = pygame.USEREVENT
 TARGET_PADDING = 30  # how many pizels off the center of the screen
-
+LIVES = 5
 class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
@@ -48,7 +51,28 @@ def draw(win, targets):
 
     for target in targets:
         target.draw(win)
-    pygame.display.update()
+    
+
+def format_time(secs):
+    milli = math.floor(int(secs*1000 %1000)/100)
+    seconds = int(round(secs % 60,1))
+    minutes = int(secs//60)
+
+    return f"{minutes:02d}:{seconds:02d}.{milli}"
+
+
+def draw_top_bar(win,elapsed_time,targets_pressed,misses): 
+    pygame.draw.rect(win,"grey",(0,0,WIDTH,TOP_BAR_HEIGHT)) 
+    time_label = LABEL_FONT.render(
+        f"Time:{format_time(elapsed_time)}",1,"black")
+    speed = round(targets_pressed/elapsed_time,1)
+    speed_label = LABEL_FONT.render(f"Speed:{speed}t/s",1,"black")
+    hits_label = LABEL_FONT.render(f"Hits:{targets_pressed}",1,"black")
+    lives_label = LABEL_FONT.render(f"Lives;{LIVES - misses}",1,"black")
+    win.blit(time_label,(5,5))
+    win.blit(speed_label,(200,5)) 
+    win.blit(hits_label,(450,5))
+    win.blit(lives_label,(650,5)) 
 def main():
     run = True
     targets = []
@@ -62,6 +86,7 @@ def main():
     while run:
         clock.tick(60) #because the targets update by frames the more frames there are the harder it becomes
         click=False
+        elapsed_time=time.time()-start_time
 
         for event in pygame.event.get():
             if event.type== pygame.QUIT:
@@ -69,7 +94,7 @@ def main():
                 break
             if event.type == TARGET_EVENT:
                 x=random.randint(TARGET_PADDING,WIDTH-TARGET_PADDING) #preventing target from going of screen because of radius
-                y=random.randint(TARGET_PADDING,HEIGHT-TARGET_PADDING)
+                y=random.randint(TARGET_PADDING+TOP_BAR_HEIGHT,HEIGHT-TARGET_PADDING)
                 target=Target(x,y)
                 targets.append(target)
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -84,9 +109,15 @@ def main():
             if click and target.collide(*mouse_pos):  # * breaks down tuple into its individual components. The collide function is not taking a tuple but the pygame.mouse.get_pos gives thex,y as a tuple so I need to separate them
                 targets.remove(target)
                 target_pressed+=1
+        if misses>= LIVES:
+            pass
+
                 
         draw(WIN,targets)
+        draw_top_bar(WIN,elapsed_time,target_pressed,misses)
+        pygame.display.update()
     pygame.quit()
 
 if __name__ == "__main__": #makes sure the function main runs if we are exucuting this file directly
-    main()
+    main()  
+
